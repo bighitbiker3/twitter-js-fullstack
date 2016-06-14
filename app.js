8ed095ee6e5943288c4794232c93f6817f661a5f
Elliott McNary <El_Is_Based@MacBook-Pro.local>
2016-06-14 15:35:54 -0400

    routes working

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var swig = require('swig');
+var routes = require('./routes/');
+
+app.use(express.static('public'));
+
+app.use('/', routes);
 
 app.engine('html', swig.renderFile);
 
@@ -15,23 +20,5 @@ app.use(function(req, res, next){
   next();
 });
 
-app.use('/special/', function(req, res, next){
-  console.log('youre special');
-  next();
-});
-
-app.get('/', function(req, res, next){
-
-  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
-
-app.get('/news', function(req, res, next){
-  res.send('This is news');
-});
-app.get('/special/:thing', function(req, res, next){
-  res.send('This is special');
-});
 
 app.listen(3000);
